c21ac32d98b7d6e3d29d1a59b5f944931d32fe90
Lorie <kch.lorie@gmail.com>
2024-12-05 16:36:38 +0100

    HTML Connexion/Formulaire

--- Site/assets/pages/connexion.php
+++ Site/assets/pages/connexion.php
@@ -14,3 +14,24 @@ if ($SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Connexion</title>
+</head>
+<body>
+    <h2>Connexion</h2>
+    <form action="" method="post">
+        <label for="identifiant">Identifiant :</label>
+        <input type="text" id="identifiant" name="identifiant" required>
+        <br><br>
+        <label for="motdepasse">Mot de passe :</label>
+        <input type="password" id="motdepasse" name="motdepasse" required>
+        <br><br>
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>
