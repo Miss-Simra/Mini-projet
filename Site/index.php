7a3980718888ec58bf53ab2c96593a8698a3b368
chag124 <charlinegautier30@gmail.com>
2024-12-09 10:05:45 +0100

    MAJ index

--- Site/index.php
+++ Site/index.php
@@ -23,14 +23,14 @@
 <header>
         <nav class="header-nav">
             <ul class="header-nav-list">
-                <li class="header-nav-item"><a class="header-nav-link" href="index.php">Accueil</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="recherche.php">Recherche</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="ajout.php">Ajout</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="connexion.php">Connexion</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="utilisateur.php">Utilisateur</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="membre.php">Membre</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="admin.php">Administration</a></li>
-                <li class="header-nav-item"><a class="header-nav-link" href="deconnexion.php">Déconnexion</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/index.php">Accueil</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/recherche.php">Recherche</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/ajout.php">Ajout</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/connexion.php">Connexion</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/utilisateur.php">Utilisateur</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/membre.php">Membre</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/admin.php">Administration</a></li>
+                <li class="header-nav-item"><a class="header-nav-link" href="assets/pages/deconnexion.php">Déconnexion</a></li>
             </ul>
         </nav>
     </header>
