8e13b45bf0f9b0b52d34b2643c7de1da57f0a735
chag124 <charlinegautier30@gmail.com>
2024-12-11 09:51:36 +0100

    MAJ connexion

--- Site/assets/pages/connexion.php
+++ Site/assets/pages/connexion.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/pages/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Connexion</title>
 </head>
 <body>
