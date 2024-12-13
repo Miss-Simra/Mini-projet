725e33dc9ebaa143b969596b317a51c02bd65ba8
Miss-Simra <simraketheeswaran@gmail.com>
2024-12-13 11:56:59 +0100

    MAJ

---
+++ Site/assets/pages/admin.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Gestion : identifiants et mots de passe des admin
+
+$admins_predefinis = ['1', 'Bibliothecaire', 'i48Dha6P',
+2, 'Admin 1', '',
+3, 'Admin 2', '',
+4, 'Admin 3', '',
+5, 'Admin 4', '']; 
+
+
+
+?>
---
+++ Site/assets/pages/utilisateur.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Gestion : identifiants et mots de passe des utilisateurs
+
+$admins_predefinis = [6, 'Utilisateur A', '',
+7, 'Utilisateur B', '',
+8, 'Utilisateur C', '',
+9, 'Utilisateur D', '',
+10, 'Utilisateur E', '',
+11, 'Utilisateur F', '',
+12, 'Utilisateur G', '',
+13, 'Utilisateur H', '',
+14, 'Utilisateur I', '',
+15, 'Utilisateur J', '',
+16, 'Utilisateur K', '',
+17, 'Utilisateur L', '',
+18, 'Utilisateur M', '',
+19, 'Utilisateur N', '']; 
+
+
+?>
